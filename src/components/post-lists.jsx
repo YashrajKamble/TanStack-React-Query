93e97483cf37dfd23d327ff75968bc3f64d0bd19
Yashraj_K <yashrajkamble7727@gmail.com>
2026-04-15 17:49:22 +0530

    useMutation Hook

--- src/components/post-lists.jsx
+++ src/components/post-lists.jsx
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchPosts } from "../api/api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { addPost, fetchPosts, fetchTags } from "../api/api";
 
 const PostLists = () => {
   const {
@@ -11,11 +11,63 @@ const PostLists = () => {
     queryKey: ["posts"],
     queryFn: fetchPosts,
   });
-
   //   console.log(data, isLoading, status, isError, error);
+
+  const { data: tagsData } = useQuery({
+    queryKey: ["tags"],
+    queryFn: fetchTags,
+  });
+
+  const {
+    mutate,
+    isError: isPostError,
+    isPending,
+    error: postError,
+    reset,
+  } = useMutation({
+    mutationFn: addPost,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const title = formData.get("title");
+    const tags = Array.from(formData.keys()).filter(
+      (key) => formData.get(key) === "on",
+    );
+
+    if (!title || !tags) return;
+    mutate({ id: postData.length + 1, title, tags });
+    e.target.reset();
+
+    // console.log(title, tags);
+  };
 
   return (
     <div className="container">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter your post.."
+          className="postbox"
+          name="title"
+        />
+        <div className="tags">
+          {tagsData?.map((tag) => {
+            // const inputId = `tag-${tag.id}`;
+
+            return (
+              <div key={tag.id}>
+                <input name={tag.name} id={tag.id} type="checkbox" />
+                <label htmlFor={tag.id}>{tag.name}</label>
+              </div>
+            );
+          })}
+        </div>
+        <button className="postbox">Post</button>
+      </form>
+
       {isLoading && <p>Loading...</p>}
       {isError && <p>{error?.message}</p>}
       {postData.map((post) => {
