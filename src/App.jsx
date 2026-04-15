98f5f75efc8e0f33a658350c0092acefde2a9985
Yashraj_K <yashrajkamble7727@gmail.com>
2026-04-15 16:06:11 +0530

    Query Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "./api/api";
 
 function App() {
+  const { data, isLoading, isError, error, status } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
 
-  useQuery(
-    {
-      queryKey:["posts"],
-      queryFn;
-    }
-  )
+  console.log(data, isLoading, status, isError, error);
   return (
     <>
       <h1>Get started</h1>
